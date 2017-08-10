ee6c823389617b1ce92ca45c35503811574848f5
#云淡然 <cloudcome@163.com>
2017-08-10 10:25:38 +0800

    + 单元测试

--- test/test.index.js
+++ test/test.index.js
@@ -7,77 +7,128 @@
 
 'use strict';
 
-var lang = require('../src/index.js');
+var validator = require('../src/index.js');
 
 describe('index.js', function () {
     it('.isHTTP', function (done) {
-        expect(lang.isHTTP()).toBe(false);
-        expect(lang.isHTTP('http://aba.com')).toBe(true);
+        expect(validator.isHTTP()).toBe(false);
+        expect(validator.isHTTP('http://aba.com')).toBe(true);
         done();
     });
 
     it('.isEmail', function (done) {
-        expect(lang.isEmail()).toBe(false);
-        expect(lang.isEmail('http://aba.com')).toBe(false);
-        expect(lang.isEmail('http@aba.com')).toBe(true);
-        expect(lang.isEmail('ht.tp@aba.com')).toBe(true);
+        expect(validator.isEmail()).toBe(false);
+        expect(validator.isEmail('http://aba.com')).toBe(false);
+        expect(validator.isEmail('http@aba.com')).toBe(true);
+        expect(validator.isEmail('ht.tp@aba.com')).toBe(true);
         done();
     });
 
     it('.isMobile', function (done) {
-        expect(lang.isMobile()).toBe(false);
-        expect(lang.isMobile('18112345678')).toBe(true);
-        expect(lang.isMobile('11112345678')).toBe(false);
+        expect(validator.isMobile()).toBe(false);
+
+        expect(validator.isMobile('13012345678')).toBe(true);
+        expect(validator.isMobile('13112345678')).toBe(true);
+        expect(validator.isMobile('13212345678')).toBe(true);
+        expect(validator.isMobile('13312345678')).toBe(true);
+        expect(validator.isMobile('13412345678')).toBe(true);
+        expect(validator.isMobile('13512345678')).toBe(true);
+        expect(validator.isMobile('13612345678')).toBe(true);
+        expect(validator.isMobile('13712345678')).toBe(true);
+        expect(validator.isMobile('13812345678')).toBe(true);
+        expect(validator.isMobile('13912345678')).toBe(true);
+
+        expect(validator.isMobile('14512345678')).toBe(true);
+        expect(validator.isMobile('14712345678')).toBe(true);
+
+        expect(validator.isMobile('15012345678')).toBe(true);
+        expect(validator.isMobile('15112345678')).toBe(true);
+        expect(validator.isMobile('15212345678')).toBe(true);
+        expect(validator.isMobile('15312345678')).toBe(true);
+        expect(validator.isMobile('15412345678')).toBe(false);
+        expect(validator.isMobile('15512345678')).toBe(true);
+        expect(validator.isMobile('15612345678')).toBe(true);
+        expect(validator.isMobile('15712345678')).toBe(true);
+        expect(validator.isMobile('15812345678')).toBe(true);
+        expect(validator.isMobile('15912345678')).toBe(true);
+
+        expect(validator.isMobile('16612345678')).toBe(true);
+
+        expect(validator.isMobile('17012345678')).toBe(true);
+        expect(validator.isMobile('17112345678')).toBe(false);
+        expect(validator.isMobile('17212345678')).toBe(false);
+        expect(validator.isMobile('17312345678')).toBe(true);
+        expect(validator.isMobile('17412345678')).toBe(false);
+        expect(validator.isMobile('17512345678')).toBe(false);
+        expect(validator.isMobile('17612345678')).toBe(true);
+        expect(validator.isMobile('17712345678')).toBe(true);
+        expect(validator.isMobile('17812345678')).toBe(true);
+        expect(validator.isMobile('17912345678')).toBe(false);
+
+        expect(validator.isMobile('18012345678')).toBe(true);
+        expect(validator.isMobile('18112345678')).toBe(true);
+        expect(validator.isMobile('18212345678')).toBe(true);
+        expect(validator.isMobile('18312345678')).toBe(true);
+        expect(validator.isMobile('18412345678')).toBe(true);
+        expect(validator.isMobile('18512345678')).toBe(true);
+        expect(validator.isMobile('18612345678')).toBe(true);
+        expect(validator.isMobile('18712345678')).toBe(true);
+        expect(validator.isMobile('18812345678')).toBe(true);
+        expect(validator.isMobile('18912345678')).toBe(true);
+
+        expect(validator.isMobile('19812345678')).toBe(true);
+        expect(validator.isMobile('19912345678')).toBe(true);
+
         done();
     });
 
     it('.isIP', function (done) {
-        expect(lang.isIP()).toBe(false);
-        expect(lang.isIP('11.11.111.11')).toBe(true);
-        expect(lang.isIP('222.233.455.11')).toBe(false);
+        expect(validator.isIP()).toBe(false);
+        expect(validator.isIP('11.11.111.11')).toBe(true);
+        expect(validator.isIP('222.233.455.11')).toBe(false);
         done();
     });
 
     it('.isNumber', function (done) {
-        expect(lang.isNumber()).toBe(false);
-        expect(lang.isNumber('11.11')).toBe(true);
-        expect(lang.isNumber('11')).toBe(true);
-        expect(lang.isNumber('-1')).toBe(true);
-        expect(lang.isNumber('-1.1')).toBe(true);
-        expect(lang.isNumber('-0.1')).toBe(true);
-        expect(lang.isNumber('-0.0')).toBe(false);
-        expect(lang.isNumber('-0.10000')).toBe(false);
-        expect(lang.isNumber('-0000.1')).toBe(false);
-        expect(lang.isNumber('1.5.11')).toBe(false);
-        expect(lang.isNumber('1.303')).toBe(true);
-        expect(lang.isNumber('1.313')).toBe(true);
-        expect(lang.isNumber('1e+1')).toBe(true);
-        expect(lang.isNumber('1.1e+1')).toBe(true);
-        expect(lang.isNumber('1e1')).toBe(true);
-        expect(lang.isNumber('1.1e1')).toBe(true);
-        expect(lang.isNumber('1e-12')).toBe(true);
-        expect(lang.isNumber('1.1e-12')).toBe(true);
+        expect(validator.isNumber()).toBe(false);
+        expect(validator.isNumber('11.11')).toBe(true);
+        expect(validator.isNumber('11')).toBe(true);
+        expect(validator.isNumber('-1')).toBe(true);
+        expect(validator.isNumber('-1.1')).toBe(true);
+        expect(validator.isNumber('-0.1')).toBe(true);
+        expect(validator.isNumber('-0.0')).toBe(false);
+        expect(validator.isNumber('-0.10000')).toBe(false);
+        expect(validator.isNumber('-0000.1')).toBe(false);
+        expect(validator.isNumber('1.5.11')).toBe(false);
+        expect(validator.isNumber('1.303')).toBe(true);
+        expect(validator.isNumber('1.313')).toBe(true);
+        expect(validator.isNumber('1e+1')).toBe(true);
+        expect(validator.isNumber('1.1e+1')).toBe(true);
+        expect(validator.isNumber('1e1')).toBe(true);
+        expect(validator.isNumber('1.1e1')).toBe(true);
+        expect(validator.isNumber('1e-12')).toBe(true);
+        expect(validator.isNumber('1.1e-12')).toBe(true);
         done();
     });
 
     it('.isInteger', function (done) {
-        expect(lang.isInteger()).toBe(false);
-        expect(lang.isInteger(0)).toBe(true);
-        expect(lang.isInteger(1)).toBe(true);
-        expect(lang.isInteger(-1)).toBe(true);
-        expect(lang.isInteger(+1)).toBe(true);
-        expect(lang.isInteger('10')).toBe(true);
-        expect(lang.isInteger('01')).toBe(false);
+        expect(validator.isInteger()).toBe(false);
+        expect(validator.isInteger(0)).toBe(true);
+        expect(validator.isInteger(1)).toBe(true);
+        expect(validator.isInteger(-1)).toBe(true);
+        expect(validator.isInteger(+1)).toBe(true);
+        expect(validator.isInteger('10')).toBe(true);
+        expect(validator.isInteger('01')).toBe(false);
         done();
     });
 
     it('.isIDCard', function (done) {
-        expect(lang.isIDCard(''.concat('341341', '1999', '01','01', '0000'))).toBe(true);
-        expect(lang.isIDCard(''.concat('341341', '1999', '01','01', '000x'))).toBe(true);
-        expect(lang.isIDCard(''.concat('341341', '1999', '01','01', '000X'))).toBe(true);
-        expect(lang.isIDCard(''.concat('341341', '1199', '01','01', '0000'))).toBe(false);
-        expect(lang.isIDCard(''.concat('341341', '1999', '21','01', '0000'))).toBe(false);
-        expect(lang.isIDCard(''.concat('341341', '1999', '11','12', '00xx'))).toBe(false);
+        expect(validator.isIDCard(''.concat('341341', '1999', '01','01', '0000'))).toBe(true);
+        expect(validator.isIDCard(''.concat('341341', '1999', '01','01', '000x'))).toBe(true);
+        expect(validator.isIDCard(''.concat('341341', '1999', '01','01', '000X'))).toBe(true);
+        expect(validator.isIDCard(''.concat('341341', '1199', '01','01', '0000'))).toBe(false);
+        expect(validator.isIDCard(''.concat('341341', '1999', '21','01', '0000'))).toBe(false);
+        expect(validator.isIDCard(''.concat('341341', '1999', '11','12', '00xx'))).toBe(false);
         done();
     });
 });
